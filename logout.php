7004170351e8bec488aa6533154ee7bf152286e5
HirotoKoyama <74949528+HirotoKoyama@users.noreply.github.com>
2020-12-08 15:08:06 +0900

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ALL & ~E_NOTICE);
-
-session_start();
-$_SESSION = array();//セッションの中身をすべて削除
-session_destroy();//セッションを破壊
-?>
-ログアウトしました。<br />
-<a href="login_form.php">ログインへ</a>
